f1d67fc735f43b1e97ad491fecda76f61fc761ef
shezray <sincerelyshez@gmail.com>
2025-04-10 15:21:41 -0500

    added cart context and shopping icon

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { UserProvider } from './contexts/user.context';
 import { ProductsProvider } from './contexts/products.context';
-
+import { CartProvider } from './contexts/cart.context';
 import './index.scss';
 
 const rootElement = document.getElementById('root');
@@ -17,7 +17,9 @@ root.render(
     <BrowserRouter>
       <UserProvider>
         <ProductsProvider>
+          <CartProvider>
           <App />
+          </CartProvider>
         </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
