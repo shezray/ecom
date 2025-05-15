7d1330c1e03e835cef59596188f2d5f94bd2f3f4
shezray <sincerelyshez@gmail.com>
2025-05-16 01:04:04 +0530

    resolved clashing styles

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -9,15 +9,15 @@ import {
   signOut,
   onAuthStateChanged,
 } from 'firebase/auth';
-import { 
-  getFirestore, 
-  doc, 
-  getDoc, 
-  setDoc, 
-  collection, 
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
   writeBatch,
   query,
-  getDocs 
+  getDocs,
 } from 'firebase/firestore';
 
 
@@ -45,17 +45,21 @@ export const signInWithGoogleRedirect = () =>
 
 export const db = getFirestore();
 
-export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
-  const collectionRef = collection(db, collectionKey );
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd,
+  field
+) => {
+  const collectionRef = collection(db, collectionKey);
   const batch = writeBatch(db);
+
   objectsToAdd.forEach((object) => {
     const docRef = doc(collectionRef, object.title.toLowerCase());
     batch.set(docRef, object);
   });
 
   await batch.commit();
-  console.log("done");
-
+  console.log('done');
 };
 
 export const getCategoriesAndDocuments = async () => {
@@ -64,13 +68,13 @@ export const getCategoriesAndDocuments = async () => {
 
   const querySnapshot = await getDocs(q);
   const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
-  const { title, items } = docSnapshot.data();
-  acc[title.toLowerCase()] = items;
-  return acc;
-}, {});
-return categoryMap;
+    const { title, items } = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {});
 
-}
+  return categoryMap;
+};
 
 export const createUserDocumentFromAuth = async (
   userAuth,
